c4978b44227db84ffd7feef90e5cfc7df8a97070
Sirojkhuja Muminkhodjaev <sirojxuja@gmail.com>
2021-12-15 20:30:29 +0500

    First commit

---
+++ src/main/PrintBarcode.java
@@ -0,0 +1,19 @@
+package main;
+
+import com.github.anastaciocintra.escpos.EscPos;
+import com.github.anastaciocintra.output.PrinterOutputStream;
+
+import javax.print.PrintService;
+import java.io.IOException;
+
+public class PrintBarcode {
+    public static void main(String[] args) throws IOException {
+        PrintService printService = PrinterOutputStream.getPrintServiceByName("xprinter");
+        PrinterOutputStream printerOutputStream = new PrinterOutputStream(printService);
+        EscPos escpos = new EscPos(printerOutputStream);
+        escpos.writeLF("Hello Wold");
+        escpos.feed(5);
+        escpos.cut(EscPos.CutMode.FULL);
+        escpos.close();
+    }
+}
